0ed149bf2b99b653c97d3d93130be92850da2854
Daniel <danielfdoficial@gmail.com>
2021-08-12 21:15:51 -0300

    add skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DiJsBadge } from "react-icons/di";
+import { DiJsBadge, DiReact } from "react-icons/di";
 
 export default function Skills() {
   function showSkills() {
@@ -16,7 +16,16 @@ export default function Skills() {
   }
     return (
       <div className='skills'>
-        <DiJsBadge />
+        <div className="skills-list1">
+          <div className="skill-list-item">
+            <DiJsBadge className="js-icon" />
+            <span>JavaScript</span>
+          </div>
+          <div className="skill-list-item">
+            <DiReact className="react-icon" /> 
+            <span>React</span>
+          </div>
+        </div>
       </div>
     );
 }
