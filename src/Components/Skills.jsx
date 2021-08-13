7a35f23e6b68893785b6a52394f71dc8a20583b8
Daniel <danielfdoficial@gmail.com>
2021-08-13 10:38:21 -0300

    add skill icons list

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import { DiJsBadge, DiReact } from "react-icons/di";
+import { DiJsBadge, DiReact, DiHtml5, DiCss3, DiGit } from "react-icons/di";
+import { SiJest, SiRedux } from "react-icons/si";
 
 export default function Skills() {
- /*  function showSkills() {
-    const mySkills = <div className="skill-list">
-      <p>HTML</p>
-      <p>CSS</p>
-      <p>JavaScript</p>
-      <p>React</p>
-      <p>Jest</p>
-    </div>
-    this.setState(() => ({
-      skills: mySkills,
-    }))
-  } */
     return (
       <div className='skills'>
         <div className="skills-list1">
           <div className="skill-list-item">
-            <DiJsBadge className="js-icon" />
+            <DiHtml5 className="icons" /> 
+            <span>HTML</span>
+          </div>
+          <div className="skill-list-item">
+            <DiCss3 className="icons" /> 
+            <span>CSS</span>
+          </div>
+          <div className="skill-list-item">
+            <DiJsBadge className="icons" />
             <span>JavaScript</span>
           </div>
           <div className="skill-list-item">
-            <DiReact className="react-icon" /> 
+            <DiReact className="icons" /> 
             <span>React</span>
+          </div>
+          <div className="skill-list-item">
+            <SiRedux className="icons" /> 
+            <span>Redux</span>
+          </div>
+          <div className="skill-list-item">
+            <SiJest className="icons" /> 
+            <span>Jest/RTL</span>
+          </div>
+          <div className="skill-list-item">
+            <DiGit className="icons" /> 
+            <span>GIT</span>
           </div>
         </div>
       </div>
